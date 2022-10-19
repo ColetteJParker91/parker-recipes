43f05f2a65925bf0b5188b117a1ee6a329eca392
ColetteJParker91 <103423447+ColetteJParker91@users.noreply.github.com>
2022-10-20 01:03:16 +0200

    Added api on to first component

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Popular() {
-  return <div>Popular</div>;
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+  const getPopular = async () => {
+    const api = await fetch(
+      "https://api.spoonacular.com/recipes/random?apiKey=39ffbf8f18ea41448abf9dfa1abef810&number=9"
+    );
+    const data = await api.json();
+    setPopular(data.recipes);
+  };
+  return (
+    <div>
+      {popular.map((recipe) => {
+        return (
+          <div>
+            <p>{recipe.title}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Popular;
